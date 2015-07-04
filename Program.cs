27eb9e86ef007f9912634a416a55f92f4039f45e
crazylf2 <tanglingmong@hotmail.com>
2015-07-04 15:44:15 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -62,7 +62,7 @@ namespace Syndra
             Q.SetSkillshot(0.6f, 125f, float.MaxValue, false, SkillshotType.SkillshotCircle);
             W.SetSkillshot(0.25f, 140f, 1600f, false, SkillshotType.SkillshotCircle);
             E.SetSkillshot(0.25f, (float)(45 * 0.5), 2500f, false, SkillshotType.SkillshotCircle);
-            EQ.SetSkillshot(0.98f, 55f, 1900f, false, SkillshotType.SkillshotLine);
+            EQ.SetSkillshot(0.98f, 65f, 1900f, false, SkillshotType.SkillshotLine);
 
             SpellList.Add(Q);
             SpellList.Add(W);
